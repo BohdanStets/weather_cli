b299cd24a70d9e7e6b50921faffd0aec21465072
Bohdan <stetsenko.bogdan.work@gmail.com>
2022-05-11 14:53:39 +0300

    Add function to save token

---
+++ services/log.service.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+const printError = (err) => {
+  console.log(chalk.bgRed('ERROR') + '---' + err);
+};
+const printSuccess = (msg) => {
+  console.log(chalk.bgGreen('SUCCESS') + '---' + msg);
+};
+const printHelp = () => {
+  console.log(
+    `
+     ${chalk.bgCyan('HELP')}
+     Без параметрів - вивід погоди
+     -s [CITY] для встановлення міста
+     -h для виводу допомоги
+     -t [API_KEY] для збереження токену
+     `
+  );
+};
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Токен збережений!');
+  } catch (err) {
+    printError(`Помилка збереження токену! ${err.message}`);
+  }
+};
 const initCLI = () => {
   const args = getArgs(process.argv);
-  if(args.h){
-     //help
+  if (args.h) {
+    printHelp();
   }
-  if(args.s){
-     //save sity
+  if (args.s) {
+    //save sity
   }
-  if(args.t){
-     //save token
+  if (args.t) {
+    saveToken(args.t);
   }
   //show info
 };
